df5279f5acf51a5d04d088830812e9cdedc0085e
Alon Shirion <shirion.alon@gmail.com>
2020-05-18 23:51:19 +0300

    shit base

--- packages/reversi-types/index.d.ts
+++ packages/reversi-types/index.d.ts
@@ -8,19 +8,41 @@ export interface User extends Document {
   date: string;
 }
 
-export const EVENT_GAME_CREATED = 'game-created'
-export const EVENT_GAME_JOINED = 'game-joined'
-export const EVENT_STATE_CHANGED = 'game-state-changed'
-export const EVENT_GAME_FINISHED = 'game-finished'
+export const EVENT_GAME_CREATED = 'game-created';
+export const EVENT_GAME_JOINED = 'game-joined';
+export const EVENT_STATE_CHANGED = 'game-state-changed';
+export const EVENT_GAME_FINISHED = 'game-finished';
 
-interface Game extends document {
-  name: string
-  readonly whitePlayer: Player
+
+const gameStatus = {
+  STATUS_FINISHED: 'finished',
+  STATUS_PENDING: 'pending',
+  STATUS_WAITING_FOR_OPPONENT: 'waiting for opponent'
+}
+
+type GameType = 'MATCHMAKING' | 'PRIVATE' | 'CPU';
+type GameStatus = 'WAITING' | 'DURING' | 'FINISHED';
+
+enum Cell {
+  BLANK = 0,
+  WHITE = 1,
+  BLACK = 2
+}
+
+interface Game extends Document {
+  name: string,
+  type: GameType,
+  whitePlayer: Player,
   blackPlayer: Player | undefined,
   status: string,
-  turn: Disc,
-  board: number[]
+  turn: 'WHITE' | 'BLACK',
+  board: Cell[],
 }
+
+type Player = Pick<User, '_id' | 'username'>;
+
+const x: Player = {asd: fsdcsdc};
+
 
 type Board = number[][]
 
@@ -34,3 +56,39 @@ interface Position {
   readonly x: number
   readonly y: number
 }
+
+
+// interface Game extends Document {
+//   name: string,
+//   type: GameType,
+//   whitePlayer: Player,
+//   blackPlayer: Player | undefined,
+//   status: string,
+//   turn: 'WHITE' | 'BLACK',
+//   board: Cell[],
+// }
+//
+//
+// // SERVER
+// interface Game extends Document {
+//   name: string,
+//   type: GameType,
+//   whitePlayer: Player,
+//   blackPlayer: Player | undefined,
+//   status: GameStatus,
+//   turn: Player._id | undefined,
+//   winner: Player._id | undefined,
+//   board: Cell[],
+// }
+//
+// type EndGameStates = 'WIN' | 'LOSS' | 'TIE';
+//
+// type ClientGameStatus = '' | 'WAITING_FOR_OPPONENT' | 'PLAYING' | EndGameStates;
+//
+// // Client
+// interface Game extends Documents {
+//   name: string;
+//   status: ClientGameStatus;
+//   enemyName: string;
+//   board: Cell[];
+// }
