86a906dc5dadafcf3405157fb8c5c07fe7f7902e
AmitFeldman <amit123feldman@gmail.com>
2020-07-21 13:05:53 +0300

    input props

--- packages/client/src/components/LabeledInput/LabeledInput.tsx
+++ packages/client/src/components/LabeledInput/LabeledInput.tsx
@@ -10,9 +10,8 @@ interface LabeledInputProps
 
 const LabeledInput: React.FC<LabeledInputProps> = ({
   label,
-  value,
-  type,
   onValueChange,
+  ...inputProps
 }) => {
   return (
     <div className="md:flex md:items-center mb-6">
@@ -24,8 +23,7 @@ const LabeledInput: React.FC<LabeledInputProps> = ({
       <div className="md:w-2/3">
         <input
           className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          type={type}
-          value={value}
+          {...inputProps}
           onChange={(e: React.FormEvent<HTMLInputElement>) => {
             onValueChange(e);
           }}
